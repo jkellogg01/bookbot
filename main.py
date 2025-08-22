8333a8d319c0a59b04999324a152b774e47c574d
Joshua Kellogg <josh@jkellogg.net>
2025-08-21 22:19:41 -0600

    pretty printed report

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, sort_chars
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     word_count = get_num_words(text)
-    print(f"{word_count} words found in the document")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
     char_counts = get_num_chars(text)
-    print(char_counts)
+    chars_sorted = sort_chars(char_counts)
+    for char_dict in chars_sorted:
+        char = char_dict["char"]
+        if not char.isalpha():
+            continue
+        count = char_dict["num"]
+        print(f"{char}: {count}")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -10,3 +10,17 @@ def get_num_chars(text):
             counts[char] = 0
         counts[char] += 1
     return counts
+
+def sort_chars(counts):
+    char_dicts = []
+    for key in counts:
+        count = counts[key]
+        char_dicts.append({
+            "char": key,
+            "num": count,
+            })
+    char_dicts.sort(reverse=True, key=sort_chars_on)
+    return char_dicts
+
+def sort_chars_on(char_count):
+    return char_count["num"]
