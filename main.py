2bc86bf9192598e331b2de36a7ad4c8d21566793
Joshua Kellogg <josh@jkellogg.net>
2025-08-21 22:24:59 -0600

    argument passing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import sys
 from stats import get_num_words, get_num_chars, sort_chars
 
 def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 {sys.argv[0]} <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    filepath = "books/frankenstein.txt"
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
